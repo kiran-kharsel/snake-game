1c6883c5e0fc5bdedeff61dfb895de04bc550769
kiran-kharsel <kirankharsel@gmail.com>
2026-03-03 14:26:13 +0530

    feat: create food element

--- main.js
+++ main.js
@@ -3,12 +3,18 @@ const gameBoard = document.querySelector('.game-board')
 
 
 // game variable
+const gridSize = 20;
 let snake = [{x: 10, y: 10}]
+let food = generateFoodPosition()
 
 // graw game map, snake, food
 function draw(){
     // reset board element
+    gameBoard.innerHTML = '';
+
+    // draw game element
     drawSnake()
+    drawFood()
 }
 
 draw()
@@ -37,4 +43,20 @@ function createGameElement(element, className){
 function setPosition(element, position){
     element.style.gridColumn = position.x
     element.style.gridRow = position.y
+}
+
+
+// draw food
+function drawFood(){
+    const foodElement = createGameElement('div', 'food')
+    setPosition(foodElement, food)
+    gameBoard.appendChild(foodElement)
+}
+
+
+// generate food position
+function generateFoodPosition(){
+    const x = Math.floor(Math.random() * gridSize) + 1;
+    const y = Math.floor(Math.random() * gridSize) + 1;
+    return {x,y}
 }
